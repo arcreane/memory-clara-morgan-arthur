e58a9ceb30f3d0e2f757c1eebe20ba5ede6ffed0
clara.boissonnot <clara.boissonot@ecole-89.com>
2023-05-05 15:06:46 +0200

    choix niveaux

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,9 @@
 #include "./ui_mainwindow.h"
 
 #include <QPushButton>
+#include <QGroupBox>
+#include <QRadioButton>
+#include <QVBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -15,6 +18,23 @@ MainWindow::MainWindow(QWidget *parent)
     // Configurer le bouton
     startButton->setGeometry(QRect(QPoint(100, 100), QSize(200, 50))); // Position et taille
     connect(startButton, SIGNAL(clicked()), this, SLOT(startGame())); // Connecter le signal clicked() à la slot startGame()
+
+    // Créer le groupe de boutons radio pour la sélection de niveau de difficulté
+    QGroupBox *difficultyGroupBox = new QGroupBox("Difficulty", this);
+
+    // Configurer le groupe de boutons radio
+    difficultyGroupBox->setGeometry(QRect(QPoint(350, 100), QSize(200, 150))); // Position et taille
+    QRadioButton *easyRadioButton = new QRadioButton("Easy", difficultyGroupBox);
+    QRadioButton *mediumRadioButton = new QRadioButton("Medium", difficultyGroupBox);
+    QRadioButton *hardRadioButton = new QRadioButton("Hard", difficultyGroupBox);
+    easyRadioButton->setChecked(true); // Sélectionner le bouton radio "Easy" par défaut
+
+    // Ajouter les boutons radio au groupe
+    QVBoxLayout *difficultyLayout = new QVBoxLayout;
+    difficultyLayout->addWidget(easyRadioButton);
+    difficultyLayout->addWidget(mediumRadioButton);
+    difficultyLayout->addWidget(hardRadioButton);
+    difficultyGroupBox->setLayout(difficultyLayout);
 }
 void MainWindow::startGame()
 {
