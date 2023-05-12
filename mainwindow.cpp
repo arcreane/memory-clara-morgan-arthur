bcff36e97bb3e3a11a378e7462d063448b1b0db5
morgan <morgan.ribou@roth-france.fr>
2023-05-12 12:36:34 +0200

    fix timer + bugs

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,12 @@
 #include <QGroupBox>
 #include <QRadioButton>
 #include <QVBoxLayout>
+#include <QHBoxLayout>
 #include <QStyle>
 #include <QTimer>
+#include <QFrame>
 #include <QLabel>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -18,7 +21,8 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     m_cardImages.append(QPixmap(":/cartes/01.png"));
-
+    m_timer = new QTimer(this);
+    connect(m_timer, &QTimer::timeout, this, &MainWindow::updateTime);
     this->restartGame();
 }
 
@@ -35,163 +39,121 @@ void MainWindow::updateTime()
 
 void MainWindow::startGame()
 {
-    QLayout* layout = ui->centralwidget->layout();
-    if (layout != nullptr) {
-        QLayoutItem* item;
-        while ((item = layout->takeAt(0)) != nullptr) {
-            delete item->widget();
-            delete item;
-        }
-        delete layout;
-    }
-    if(m_easyRadioButton->isChecked()){
-        int ROWS = 4;
-        int COLS = 6;
-
-
-        // Ajouter le label au layout du widget_2
-        QVBoxLayout* widget2Layout = new QVBoxLayout(ui->widget_2);
-        widget2Layout->addWidget(m_labelTime);
-        ui->widget_2->setLayout(widget2Layout);
-
-        // Créer le timer
-
-        QGridLayout* gridLayout = new QGridLayout(ui->widget_1);
-        for(int i=0; i<ROWS; i++){
-            for(int j=0; j<COLS; j++){
-                QPixmap frontImage(":/cartes/01.png");
-                QPixmap backImage(":/cartes/back.png");
-                CardButton* card = new CardButton(frontImage, backImage, this);
-
-                // Add the card to the grid layout
-                gridLayout->addWidget(card, i, j);
-
-                // Connect the clicked signal of the card to the flip slot
-                connect(card, &CardButton::clicked, card, &CardButton::flip);
-            }
-        }
-        // Afficher la grille
-        m_timer->stop();
-        m_seconds = 0;
-        m_labelTime->setText("00:00");
-        m_timer->start(1000);
-        ui->widget_1->setStyleSheet("border: 2px solid black");
-        ui->widget_2->setStyleSheet("border: 2px solid black");
-        ui->centralwidget->setStyleSheet("background-color: darkgreen");
-        ui->widget_1->setLayout(gridLayout);
-
-        // Créer le bouton
-        m_restartButton = new QPushButton(ui->widget_2);
-        m_restartButton->setObjectName("restartButton");
-        m_restartButton->setFixedSize(120, 40);
-        // Configurer le bouton
-        widget2Layout->addWidget(m_restartButton);
-        connect(m_restartButton, &QPushButton::clicked, this, &MainWindow::restartGame);
+    int ROWS = 0;
+    int COLS = 0;
+    if (m_easyRadioButton->isChecked()) {
+        ROWS = 4;
+        COLS = 6;
     }
     else if(m_mediumRadioButton->isChecked()){
-        int ROWS = 4;
-        int COLS = 9;
-        QGridLayout* gridLayout = new QGridLayout(ui->centralwidget);
-        for(int i=0; i<ROWS; i++){
-            for(int j=0; j<COLS; j++){
-                QPixmap frontImage(":/cartes/01.png");
-                QPixmap backImage(":/cartes/back.png");
-                CardButton* card = new CardButton(frontImage, backImage, this);
+        ROWS = 4;
+        COLS = 8;
 
-                // Add the card to the grid layout
-                gridLayout->addWidget(card, i, j);
-
-                // Connect the clicked signal of the card to the flip slot
-                connect(card, &CardButton::clicked, card, &CardButton::flip);
-            }
-        }
-        // Afficher la grille
-        ui->centralwidget->setStyleSheet("background-color: darkgreen");
-        ui->centralwidget->setLayout(gridLayout);
     }
     else if(m_hardRadioButton->isChecked()){
-        int ROWS = 6;
-        int COLS = 8;
-        QGridLayout* gridLayout = new QGridLayout(ui->centralwidget);
-        for(int i=0; i<ROWS; i++){
-            for(int j=0; j<COLS; j++){
-                QPixmap frontImage(":/cartes/01.png");
-                QPixmap backImage(":/cartes/back.png");
-                CardButton* card = new CardButton(frontImage, backImage, this);
-
-                // Add the card to the grid layout
-                gridLayout->addWidget(card, i, j);
-
-                // Connect the clicked signal of the card to the flip slot
-                connect(card, &CardButton::clicked, card, &CardButton::flip);
-            }
-        }
-        // Afficher la grille
-        ui->centralwidget->setStyleSheet("background-color: darkgreen");
-        ui->centralwidget->setLayout(gridLayout);
+        ROWS = 6;
+        COLS = 8;
     }
-}
+    m_seconds = 0;
 
-void MainWindow::restartGame(){
-
-    // Créer le label
-    m_timer = new QTimer(this);
-    connect(m_timer, &QTimer::timeout, this, &MainWindow::updateTime);
     m_labelTime = new QLabel("00:00");
     m_labelTime->setObjectName("labelTime"); // Nommer le label
     m_labelTime->setAlignment(Qt::AlignHCenter | Qt::AlignTop); // Aligner le texte au centre
     m_labelTime->setStyleSheet("font-size: 20pt; color: white"); // Définir le style
 
-    m_timer->stop();
-    m_seconds = 0;
     m_labelTime->setText("00:00");
-
-    QLayout* layout = ui->widget_2->layout();
-    if (layout != nullptr) {
-        QLayoutItem* item;
-        while ((item = layout->takeAt(0)) != nullptr) {
-            delete item->widget();
-            delete item;
+    m_timer->start(1000);
+    // Création d'un conteneur pour les widgets
+    QWidget* widgetContainer = new QWidget(this);
+    widgetContainer->setObjectName("WidgetContainer");
+    widgetContainer->setStyleSheet("QWidget#WidgetContainer { background-color: darkgreen; }");
+    // Création des widgets à l'intérieur du conteneur
+    QWidget* widget1 = new QWidget(widgetContainer);
+    widget1->setObjectName("Widget1");
+    widget1->setFixedSize(800, 740);
+    QWidget* widget2 = new QWidget(widgetContainer);
+    widget2->setObjectName("Widget2");
+    widget2->setFixedSize(370, 740);
+    // Création d'un layout vertical pour le conteneur
+    QHBoxLayout* layout = new QHBoxLayout(widgetContainer);
+    layout->addWidget(widget1);
+    layout->addWidget(widget2);
+    // Ajout du conteneur dans la fenêtre principale
+    setCentralWidget(widgetContainer);
+    // Ajouter le label au layout du widget_2
+    QVBoxLayout* widget2Layout = new QVBoxLayout(widget2);
+    QGridLayout* gridLayout = new QGridLayout(widget1);
+    for(int i=0; i<ROWS; i++){
+        for(int j=0; j<COLS; j++){
+            QPixmap frontImage(":/cartes/01.png");
+            QPixmap backImage(":/cartes/back.png");
+            CardButton* card = new CardButton(frontImage, backImage, this);
+            gridLayout->addWidget(card, i, j);
+            connect(card, &CardButton::clicked, card, &CardButton::flip);
         }
-        delete layout;
     }
+    widget1->setStyleSheet("border: 2px solid black");
+    widget2->setStyleSheet("border: 2px solid black");
+    widget1->setLayout(gridLayout);
+    // Créer le bouton
+    m_restartButton = new QPushButton("Restart Game", widget2);
+    m_restartButton->setObjectName("restartButton");
+    m_restartButton->setFixedSize(350, 40);
+    // Ajout d'un espace vide en haut pour centrer le bouton
+    widget2Layout->addStretch(1);
+
+    // Création du bouton et ajout au widget2Layout
+    widget2Layout->addWidget(m_restartButton, 0, Qt::AlignHCenter | Qt::AlignBottom);
+
+    // Ajout d'un espace vide en bas pour pousser le bouton vers le bas
+    widget2Layout->addStretch(1);
+    // Configurer le bouton
+    widget2Layout->addWidget(m_labelTime);
+
+    widget2Layout->addWidget(m_restartButton); // Ajouter le bouton au layout
+    connect(m_restartButton, &QPushButton::clicked, this, &MainWindow::restartGame);
+}
+
+void MainWindow::restartGame(){
+
+    m_timer->stop();
 
     // Créer le bouton
-    QPushButton *startButton = new QPushButton("Start Game", this);
-
-    // Configurer le bouton
+    QPushButton* startButton = new QPushButton("Start Game", this);
     startButton->setGeometry(QRect(QPoint(350, 100), QSize(200, 150))); // Position et taille
     connect(startButton, &QPushButton::clicked, this, &MainWindow::startGame);
 
     // Créer la boîte de groupe pour la difficulté
     QGroupBox* difficultyGroupBox = new QGroupBox("Difficulty", this);
     difficultyGroupBox->setGeometry(QRect(QPoint(350, 300), QSize(200, 100))); // Position et taille
-    m_easyRadioButton = new QRadioButton("Easy", difficultyGroupBox); // Stocker un pointeur vers le bouton radio "Easy"
-    m_easyRadioButton->setObjectName("easyRadioButton"); // Nommer le bouton radio
-    m_mediumRadioButton = new QRadioButton("Medium", difficultyGroupBox); // Stocker un pointeur vers le bouton radio "medium"
-    m_mediumRadioButton->setObjectName("mediumRadioButton"); // Nommer le bouton radio
-    m_hardRadioButton = new QRadioButton("Hard", difficultyGroupBox); // Stocker un pointeur vers le bouton radio "hard"
-    m_hardRadioButton->setObjectName("hardRadioButton"); // Nommer le bouton radio
-    m_easyRadioButton->setChecked(true); // Sélectionner le bouton radio "Easy" par défaut
+    m_easyRadioButton = new QRadioButton("Easy", difficultyGroupBox);
+    m_easyRadioButton->setObjectName("easyRadioButton");
+    m_mediumRadioButton = new QRadioButton("Medium", difficultyGroupBox);
+    m_mediumRadioButton->setObjectName("mediumRadioButton");
+    m_hardRadioButton = new QRadioButton("Hard", difficultyGroupBox);
+    m_hardRadioButton->setObjectName("hardRadioButton");
+    m_easyRadioButton->setChecked(true);
 
     // Ajouter les boutons radio au groupe
-    QVBoxLayout *difficultyLayout = new QVBoxLayout;
-    difficultyLayout->addWidget(m_easyRadioButton); // Utiliser la variable membre pour ajouter le bouton radio "Easy"
+    QVBoxLayout* difficultyLayout = new QVBoxLayout(difficultyGroupBox);
+    difficultyLayout->addWidget(m_easyRadioButton);
     difficultyLayout->addWidget(m_mediumRadioButton);
     difficultyLayout->addWidget(m_hardRadioButton);
-    difficultyGroupBox->setLayout(difficultyLayout);
 
-    // Ajouter les widgets à la fenêtre
-    QVBoxLayout* mainLayout = new QVBoxLayout;
+    // Créer un widget central
+    QWidget* centralWidget = new QWidget(this);
+    QVBoxLayout* mainLayout = new QVBoxLayout(centralWidget);
     mainLayout->addWidget(startButton);
-    mainLayout->addWidget(difficultyGroupBox); // Ajouter la boîte de groupe au layout principal
-    ui->centralwidget->setLayout(mainLayout);
-    }
+    mainLayout->addWidget(difficultyGroupBox);
+
+    // Définir le widget central
+    setCentralWidget(centralWidget);
+}
 
 
 
 MainWindow::~MainWindow()
 {
     delete ui;
-    }
+}
 
